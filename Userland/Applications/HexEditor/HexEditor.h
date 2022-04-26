bbe47ee8e4702f99a7e5c670d0ea02d9f5a22d8d
MetallicSquid <gcmacneil@icloud.com>
2022-04-26 14:14:05 +0100

    HexEditor: Add HexEditor::get_selected_bytes()
    This adds a helper function that returns the bytes that the user has
    selected as a vector.

--- Userland/Applications/HexEditor/HexEditor.h
+++ Userland/Applications/HexEditor/HexEditor.h
@@ -38,6 +38,7 @@ public:
     void open_file(NonnullRefPtr<Core::File> file);
     ErrorOr<void> fill_selection(u8 fill_byte);
     Optional<u8> get_byte(size_t position);
+    Vector<u8> get_selected_bytes();
     bool save_as(NonnullRefPtr<Core::File>);
     bool save();
 
